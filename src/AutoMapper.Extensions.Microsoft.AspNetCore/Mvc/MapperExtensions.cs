7b28cc3dea271c03750b5ee0cd7c1d8e6702c3ff
Mike Cole <cole.mike@gmail.com>
2018-06-19 16:13:11 -0500

    Initial library.

---
+++ src/AutoMapper.Extensions.Microsoft.AspNetCore/Mvc/MapperExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AutoMapper.Extensions.Microsoft.AspNetCore.Mvc
+{
+    public static class MapperExtensions
+    {
+        public static OkObjectResult Ok<T>(this IMapper mapper, object value)
+        {
+            var result = Mapper.Map<T>(value);
+            return new OkObjectResult(result);
+        }
+    }
+}
---
+++ src/TestApp/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using AutoMapper.Extensions.Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IMapper _mapper;
+
+        public HomeController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public IActionResult Index()
+        {
+            var source = new Source
+            {
+                Id = 1,
+                Value = "Foo"
+            };
+
+            return _mapper.Ok<Destination>(source);
+        }
+    }
+
+    public class Source
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+    }
+
+    public class Destination
+    {
+        public string Value { get; set; }
+    }
+}
